c4411717dd09fa0409a5b565fdef9912aee7a11c
ychung333 <166780948+ychung333@users.noreply.github.com>
2026-02-27 11:07:00 -0800

    Implement basic file input/output and tokenization

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include "lexer.cpp"
+
+using namespace std;
+
+int main() {
+
+    ifstream fin;   // file input
+    ofstream fout;  // file output
+    string inputFileName;  // store text
+
+    // ask user to input test case that user want to work on
+    cout << "Enter the name of source file: ";
+    cin >> inputFileName;
+
+    fin.open(inputFileName);   // open file
+
+    // if file name is wrong or has any issue 
+    if (!fin) {
+        cout << "Error: Cannot open file." << endl;
+        return 1;
+    }
+
+    // make output file and write to it
+    string outputFileName = "fout_" + inputFileName;
+    fout.open(outputFileName);
+
+    Lexer lexer(fin);  // create lexer
+
+    Token token;  // create variable for token
+
+    // it read token and print them to output file until there is no more to read
+    while (true) {
+        token = lexer.nextToken();
+
+        if (token.type == "EOF")
+            break;
+
+        fout << token.type << "\t\t" 
+             << token.lexeme << endl;
+    }
+
+    cout << "Tokenization complete." << endl;
+    cout << "Output written to: " << outputFileName << endl;
+
+    // close input file and output file
+    fin.close();
+    fout.close();
+
+    return 0;
+}
