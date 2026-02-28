0a155df31febf764b3476b07ae9740470d84c314
Amank1243 <148727967+Amank1243@users.noreply.github.com>
2026-02-28 18:59:11 +0000

    Worked on lexer.cpp.
    Added keywords, and template for the FSM we will work on. Added test cases

--- lexer.cpp
+++ lexer.cpp
@@ -1,11 +1,25 @@
+#include "lexer.h"
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <cctype>
+#include <unordered_set>
 using namespace std;
 
-struct Token 
-{
-    string type;
-    string lexeme;
+// Removed token struct as it was already defined in lexer.h
+
+const unordered_set<string> KEYWORDS = { // using an unordered_set for quick lookup times when checking to see if a word in and because order does not matter a keyword
+    "integer", "if", "otherwise", "fi", "while", "return", "read", "write"
 };
+
+Token identifierFSM(){
+
+}
+
+Token realFSM() {
+
+}
+
+Token integerFSM() {
+    
+}
